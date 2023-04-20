01921d18f9a618d27f637f299f99eb029f877c75
Amandos Daulet <djamandos@sber-solutions.kz>
2023-04-20 15:53:25 +0600

    new waste

---
+++ src/app/manifest-form/manifest-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manifest-form',
+  templateUrl: './manifest-form.component.html',
+  styleUrls: ['./manifest-form.component.scss']
+})
+export class ManifestFormComponent {
+  
+}
